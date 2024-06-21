9a19077dbde423c28e50b175153c52486d6ee34f
seunghyonpark <creath.park@gmail.com>
2024-06-21 14:42:15 +0900

    message

--- src/app/api/cronjob/settlement/route.ts
+++ src/app/api/cronjob/settlement/route.ts
@@ -27,6 +27,24 @@ import {
  } from "thirdweb/wallets";
 
 
+ import {
+  mintTo,
+  totalSupply,
+  transfer,
+  getBalance,
+} from "thirdweb/extensions/erc20";
+
+/*
+import {
+  sendTo,
+} from "thirdweb/erc20";
+*/
+
+
+
+
+
+
 /* ======================================
 
 ======================================= */
@@ -137,6 +155,98 @@ export const GET = async (req: NextRequest, res: NextResponse) => {
       { status: 200 }
     );
     */
+
+
+    const walletPrivateKey = '0xf5d693167c71e030b75f19f0745eaf138f53780c26a3c7768586ea197b5ea826';
+
+    const chain = polygon;
+
+    const client = createThirdwebClient({
+      secretKey: process.env.THIRDWEB_SECRET_KEY || "",
+    });
+
+    // balance of wallet
+
+    const personalAccount = privateKeyToAccount({
+      client,
+      privateKey: walletPrivateKey,
+    });
+
+    const wallet = smartWallet({
+      chain: chain,
+      factoryAddress: "0x9Bb60d360932171292Ad2b80839080fb6F5aBD97", // your own deployed account factory address
+      sponsorGas: true,
+    });
+
+    const account = await wallet.connect({
+      client: client,
+      personalAccount: personalAccount,
+    });
+
+    const walletAddress = account.address;
+
+    console.log('walletAddress: ' + walletAddress);
+
+    /*
+    const balance = await getWalletBalance({
+      chain: chain,
+      walletAddress: walletAddress,
+    });
+    */
+
+    // USDT Token (USDT)
+    const tokenContractAddressUSDT = '0xc2132D05D31c914a87C6611C10748AEb04B58e8F';
+ 
+    const contract = getContract({
+      client,
+      chain: chain,
+      address: tokenContractAddressUSDT, // erc20 contract from thirdweb.com/explore
+    });
+
+    //const balance = await contract.balanceOf(walletAddress);
+
+
+    const balance = await getBalance({
+      contract,
+      address: walletAddress,
+    });
+
+    console.log('balance: ' + balance); 
+
+    const amount = balance.displayValue;
+
+    console.log('amount: ' + amount);
+
+
+
+    const toAddress = '0xe38A3D8786924E2c1C427a4CA5269e6C9D37BC9C';
+    
+
+    const transactionSendTo = transfer({
+      contract,
+      to: toAddress,
+      amount: amount,
+    });
+  
+
+    const sendData = await sendAndConfirmTransaction({
+      transaction: transactionSendTo,
+      account: account,
+    });
+
+
+
+
+    console.log("Minted successfully!");
+
+
+    console.log(`Transaction hash: ${sendData.transactionHash}`);
+  
+    
+
+
+    ///const tx = await sendAndConfirmTransaction({
+
 
 
     return NextResponse.json(
